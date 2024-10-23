80aeab79cd75487fbe50e622fb67e5668e15c1c8
Florian Romann from Dell-Fedora <s8flroma@stud.uni-saarland.de>
2024-10-23 22:51:58 +0200

    [feature] implemented assignment 1.1 perspective camera model

--- src/cameras/perspective.cpp
+++ src/cameras/perspective.cpp
@@ -13,32 +13,54 @@ namespace lightwave {
  * ray.direction.y < 0 ).
  */
 class Perspective : public Camera {
+protected:
+    float z;
+    float x_ratio = 1;
+    float y_ratio = 1;
+
 public:
     Perspective(const Properties &properties) : Camera(properties) {
-        NOT_IMPLEMENTED
+        const float fov = properties.get<float>("fov");
 
-        // hints:
         // * precompute any expensive operations here (most importantly
         // trigonometric functions)
+        // calculate the length of the z vector, must transform fov to radians
+        z = 1 / tan((fov / 2) * (Pi / 180));
+        // hints:
         // * use m_resolution to find the aspect ratio of the image
+        float y = m_resolution.y();
+        float x = m_resolution.x();
+        // TODO: truly understand why we need the if case
+        if (z >= 1)
+            x_ratio = x / y;
+        else
+            y_ratio = y / x;
     }
 
     CameraSample sample(const Point2 &normalized, Sampler &rng) const override {
-        NOT_IMPLEMENTED
+        // define vector pointing to the point on the z plane
+        Vector direction(normalized.x() * x_ratio, normalized.y() * y_ratio, z);
 
-        // hints:
+        // normalize the local ray
+        Ray local_ray(Vector(0.f, 0.f, 0.f), direction.normalized());
+
         // * use m_transform to transform the local camera coordinate system
         // into the world coordinate system
+        Ray world_ray = m_transform->apply(local_ray);
+
+        return CameraSample{ .ray = world_ray, .weight = Color(1.0f) };
     }
 
     std::string toString() const override {
-        return tfm::format("Perspective[\n"
-                           "  width = %d,\n"
-                           "  height = %d,\n"
-                           "  transform = %s,\n"
-                           "]",
-                           m_resolution.x(), m_resolution.y(),
-                           indent(m_transform));
+        return tfm::format(
+            "Perspective[\n"
+            "  width = %d,\n"
+            "  height = %d,\n"
+            "  transform = %s,\n"
+            "]",
+            m_resolution.x(),
+            m_resolution.y(),
+            indent(m_transform));
     }
 };
 
