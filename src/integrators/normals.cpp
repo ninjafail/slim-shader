4bb11009e70f0558b0df7e20d62174254151dbda
Leonie von Mann <s8levonm@stud.uni-saarland.de>
2024-11-06 11:01:16 +0100

    Adjust normals.cpp

--- src/integrators/normals.cpp
+++ src/integrators/normals.cpp
@@ -2,7 +2,7 @@
 
 namespace lightwave {
 class NormalIntegrator : public SamplingIntegrator {
-    bool m_remap;
+    bool m_remap = true;
 
 public:
     NormalIntegrator(const Properties &properties)
@@ -12,9 +12,14 @@ public:
 
     Color Li(const Ray &ray, Sampler &rng) override {
         auto intersection = m_scene->intersect(ray, rng);
-        return Color(intersection.geometryNormal);
+        Vector normal     = intersection.geometryNormal;
+        if (m_remap)
+            return (Color(normal) + Color(1)) / 2;
+        return Color(normal);
     }
 
     std::string toString() const override { return "NormalIntegrator[]"; }
 };
 } // namespace lightwave
+
+REGISTER_INTEGRATOR(NormalIntegrator, "normals")
